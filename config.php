48bc792d34e69e0d5674124cebdb4738d318c1fc
Lio MJ <liomj83@gmail.com>
2022-09-27 10:20:19 +0800

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $starting_year = '2021'; // Starting Year in index.php dropdown
 $system_dsn ='myattendance'; // System DSN - Data Source Name MS Access Database
 $access_dbuser=''; // MS Access Database User. Leave Blank if empty
-$access_dbpassword=''; // MS Access Database Password
+$access_dbpassword=''; // MS Access Database Password. Leave Blank if empty
 
 //ODBC Connection
 $conn=odbc_connect($system_dsn,$access_dbuser,$access_dbpassword);
