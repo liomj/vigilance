361be414daf9fa8635306071e3d81ce8c07f4803
Lio MJ <liomj83@gmail.com>
2022-09-27 10:21:46 +0800

    cosmetics

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //  Author: Lio MJ 
 //  URL: (https://github.com/liomj/)
-//  Description: Simple PHP/MS Access/ODBC Attendance Report For Vigilance Fingerprint Time Clock VT300 Device (VAMS 3.7.1)
+//  Description:  PHP/MS Access/ODBC Attendance Report For Vigilance Fingerprint Time Clock VT300 Device (VAMS 3.7.1)
 
 
 //Configuration
